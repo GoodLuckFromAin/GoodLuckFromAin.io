97bdb798f63886ba20acf20d9392824ac16cad12
GoodLuckFromAin <119645376+GoodLuckFromAin@users.noreply.github.com>
2022-12-02 06:04:27 +0200

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,11 +1,13 @@
 
-const quotes = ["When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.",
-                "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.",
-                "Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together."];
+const quotes = [["When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.","Efont1"],
+                ["It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.","Efont1"],
+                ["Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.","Efont1"],
+                ["إذا كنت تستطيع تخيل صورة ما فذلك يعني أنّه يمكنك أن تجعلها واقعاً، وإذا كنت تستطيع أن تحلم فهذا يعني أنّك تستطيع تحقيق حلمك","Afont1"]
+            ];
 
 const quote_box = document.getElementsByClassName("quote")[0];
-const index = Math.floor(Math.random() * 3);
+const index = Math.floor(Math.random() * quotes.length);
 
 
-quote_box.innerHTML = quotes[index];
-
+quote_box.innerHTML = quotes[index][0];
+quote_box.style.fontFamily = quotes[index][1];
